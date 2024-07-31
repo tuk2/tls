ab65e2e5109a8742bd514002e8793bd1b694a0e3
tuk2 <98948420+tuk2@users.noreply.github.com>
2024-07-31 03:00:54 +0300

    Update key_agreement.go

--- key_agreement.go
+++ key_agreement.go
@@ -167,11 +167,15 @@ type ecdheKeyAgreement struct {
 }
 
 func (ka *ecdheKeyAgreement) generateServerKeyExchange(config *Config, cert *Certificate, clientHello *clientHelloMsg, hello *serverHelloMsg) (*serverKeyExchangeMsg, error) {
-	var curveID CurveID
-	for _, c := range clientHello.supportedCurves {
-		if config.supportsCurve(c) && curveIdToCirclScheme(c) == nil { // [uTLS] ported from cloudflare/go
-			curveID = c
-			break
+	preferredCurves := config.curvePreferences()
+
+NextCandidate:
+	for _, candidate := range preferredCurves {
+		for _, c := range clientHello.supportedCurves {
+			if candidate == c {
+				ka.curveid = c
+				break NextCandidate
+			}
 		}
 	}
 
